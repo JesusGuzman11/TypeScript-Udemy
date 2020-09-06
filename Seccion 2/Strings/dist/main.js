b2656ca71af31a3649ae6c507310fdb7e3011671
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-09-06 12:55:33 -0500

    Strings TS

---
+++ Seccion 2/Strings/dist/main.js
@@ -0,0 +1,10 @@
+var batman = "Batman";
+var linternaVerde = "Linterna Verde";
+var superman = "Superman";
+console.log(batman);
+console.log(linternaVerde);
+console.log(superman);
+var concatenar = "Los héroes son:" + " " + superman + ", " + batman + ", " + linternaVerde;
+console.log(concatenar);
+var concat = "Los h\u00E9roes son: " + superman + ", " + batman + ", " + linternaVerde;
+console.log(concat);
---
+++ Seccion 2/Strings/main.ts
@@ -0,0 +1,13 @@
+let batman:string = "Batman";
+let linternaVerde:string = "Linterna Verde";
+let superman:string = `Superman`;
+
+console.log(batman);
+console.log(linternaVerde);
+console.log(superman);
+
+let concatenar = "Los héroes son:" + " " + superman + ", " + batman + ", " + linternaVerde 
+console.log(concatenar)
+
+let concat = `Los héroes son: ${superman}, ${batman}, ${linternaVerde}`
+console.log(concat)
