eb6314f08e0758be16a6f8a2a51c18ad7c4722d1
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-09-06 13:13:44 -0500

    Any Type

---
+++ Seccion 2/Any/main.ts
@@ -0,0 +1,15 @@
+let avenger:any = "Superman";
+let existe;
+let derrotas;
+
+avenger = "Dr Strange";
+console.log(avenger.charAt(0));
+
+avenger = 150.5555;
+console.log(avenger.toFixed(2));
+
+avenger = true;
+console.log(avenger);
+
+console.log(existe);
+console.log(derrotas);
