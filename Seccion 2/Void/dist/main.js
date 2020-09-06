0a7742e3b74db58108532606fd48913361784974
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-09-06 14:51:00 -0500

    Void

---
+++ Seccion 2/Void/dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function llamarBatman() {
+    console.log("Mostrar Batiseñal");
+}
+//This function returns value so we got error
+/*
+function llamarBatman():void {
+  console.log("Mostrar Batiseñal")
+  return 2
+}
+*/
---
+++ Seccion 2/Void/main.ts
@@ -0,0 +1,12 @@
+function llamarBatman(): void {
+  console.log("Mostrar Batiseñal");
+}
+
+//This function returns value so we got error
+
+/*
+function llamarBatman():void {
+  console.log("Mostrar Batiseñal")
+  return 2
+}
+*/
