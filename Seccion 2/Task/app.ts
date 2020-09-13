43da022bf9c6569879c9bf7e63aa1cad3d21b5db
Jesus Guzman <47365771+JesusGuzman11@users.noreply.github.com>
2020-09-13 16:16:09 -0500

    Task Section 2 (#15)

---
+++ Seccion 2/Task/app.ts
@@ -0,0 +1,46 @@
+// Tipos
+let batman:string = "Bruce";
+let superman:string = "Clark";
+
+let existe:boolean = false;
+
+// Tuplas
+let parejaHeroes:[string,string] = [batman,superman];
+let villano:[string,number,boolean] = ["Lex Lutor",5,true];
+
+// Arreglos
+let aliados:string[] = ["Mujer Maravilla","Acuaman","San", "Flash"];
+
+//Enumeraciones
+enum Fuerza {
+  aquaman = 0,
+  batman = 1,
+  flash = 5,
+  superman = 100,
+}
+
+let fuerzaFlash = Fuerza.flash;
+let fuerzaSuperman = Fuerza.superman;
+let fuerzaBatman = Fuerza.batman;
+let fuerzaAquaman = Fuerza.aquaman;
+
+
+// Retorno de funciones
+function activar_batiseñal():string{
+  return "activada";
+}
+
+function pedir_ayuda():void{
+  console.log("Auxilio!!!");
+}
+
+// Aserciones de Tipo
+let poder:string = "100";
+let largoDelPoder:number = (<string>poder).length;
+console.log( largoDelPoder );
+
+//Alternative
+
+let poder2:number = 100;
+let largoDelPoder2:number = poder2.toString().length;
+console.log( largoDelPoder2 );
