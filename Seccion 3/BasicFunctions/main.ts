296946bf697f974e069d8ae9a88b9da4a28f9e90
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-09-13 16:34:24 -0500

    Basic Functions

---
+++ Seccion 3/BasicFunctions/main.ts
@@ -0,0 +1,12 @@
+let heroe: string = "Flash";
+
+function imprimirHeroe(): string {
+  return heroe;
+}
+
+function llamarBatman(): string {
+  return "Batiseñal activada";
+}
+
+console.log(imprimirHeroe());
+console.log(llamarBatman());
