2eef261ab0baa4760ba6bb9d318255a4ecee7ce9
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-10-09 16:10:04 -0500

    Revert "Section 2 Completed (#24)"
    This reverts commit ef9ba08ff593eeff16934252b514531673c373d5.

--- Seccion 3/OptionalParams/dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function nombreCompleto(nombre, apellido) {
-    if (apellido) {
-        return nombre + " " + apellido;
-    }
-    return "" + nombre;
-}
-var nombre = nombreCompleto("Barry", "Allen");
-console.log(nombre);
