b4f1712cf33361e3b0597165a1ff2b055969baf8
Jesus <jesusguzman.0311@gmail.com>
2020-10-20 17:52:35 -0500

    Multiple Types

---
+++ Seccion 4/MultipleTypes/dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var test = "Jesus";
+test = 10;
+test = {
+    nombre: "Annie",
+    edad: 26,
+    poderes: ["Super Mujer", "Inteligencia"],
+    getNombre: function () {
+        return "Annie";
+    },
+};
+test.getNombre;
