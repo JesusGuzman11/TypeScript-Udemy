99cadff458e0f7dec73622fc1622deb10cf1ccf9
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-10-22 00:25:43 -0500

    Main Map

--- Seccion 5/SourceMap/main.js
+++ Seccion 5/SourceMap/main.js
@@ -2,8 +2,6 @@
 var nombre = "Jesus";
 var edad = 30;
 function imprimir(nombre, edad) {
-    nombre = nombre.toLowerCase();
-    edad = edad + 10;
     return "Tu nombre es: " + nombre + " y la edad es " + edad;
 }
 var mensaje = imprimir(nombre, edad);
--- Seccion 5/SourceMap/main.ts
+++ Seccion 5/SourceMap/main.ts
@@ -2,8 +2,6 @@ let nombre: string = "Jesus";
 let edad: number = 30;
 
 function imprimir(nombre: string, edad: number): string {
-  nombre = nombre.toLowerCase();
-  edad = edad + 10;
   return `Tu nombre es: ${nombre} y la edad es ${edad}`;
 }
 
