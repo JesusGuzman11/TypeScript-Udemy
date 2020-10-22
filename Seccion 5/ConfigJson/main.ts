3de747385afee126b09abacb56ae73b54c8ef237
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-10-21 23:47:21 -0500

    Ts Config Rules

---
+++ Seccion 5/ConfigJson/main.ts
@@ -0,0 +1,6 @@
+let heroe: string = "Superman";
+let edad: number = 30;
+
+/* let prueba:number = undefined => Esto lanza un error por la propiedad agregada al tsconfig */
+
+let prueba: undefined = undefined;
