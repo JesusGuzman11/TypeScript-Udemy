69df2326d50060fd545d44bd2879bc702d3d73e1
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-11-01 16:04:03 -0600

    Let

---
+++ Seccion 6/Let/dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+var nombre = "Jesus";
+// let nombre = "Jesus";
+if (true) {
+    var nombre_1 = "Annie";
+    if (true) {
+        var nombre_2 = "Tirsa";
+    }
+}
+console.log(nombre);
