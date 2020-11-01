dd2e24749d4746b3121a513505696de0e51c7e07
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-11-01 16:18:34 -0600

    Const

---
+++ Seccion 6/Const/dist/main.js
@@ -0,0 +1,15 @@
+"use strict";
+// const OPCION: string = "Activo";
+var OPCIONES = {
+    activado: true,
+    desactivado: false
+};
+OPCIONES.desactivado = true;
+console.log(OPCIONES);
+if (true) {
+    var OPCION = "Desavtivado";
+}
+for (var _i = 0, _a = [1, 2, 3, 4, 5, 6]; _i < _a.length; _i++) {
+    var i = _a[_i];
+    console.log(i);
+}
---
+++ Seccion 6/Const/main.ts
@@ -0,0 +1,18 @@
+// const OPCION: string = "Activo";
+
+const OPCIONES = {
+  activado: true,
+  desactivado: false
+}
+
+OPCIONES.desactivado = true
+
+console.log(OPCIONES)
+
+if (true) {
+  const OPCION: string = "Desavtivado";
+}
+
+for (const i of [1, 2, 3, 4, 5, 6]) {
+  console.log(i);
+}
