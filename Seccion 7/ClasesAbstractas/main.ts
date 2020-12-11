5af6746b2b9ae0a72a37b55f4ca841d927d794ae
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-12-11 17:27:26 -0600

    Class Abstract

---
+++ Seccion 7/ClasesAbstractas/main.ts
@@ -0,0 +1,11 @@
+/* Clases Abstractas */
+
+abstract class Mutantes {
+  constructor(public nombre: string, public nombreReal: string) {}
+}
+
+class Xmen extends Mutantes {}
+
+let wolverine = new Xmen("Wolverine", "Logan");
+
+console.log(wolverine);
