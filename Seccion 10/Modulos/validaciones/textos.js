57019292bbeaeb20377f0358feb7d384561a075f
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-12-29 19:09:29 -0600

    Lite server and gitignore

---
+++ Seccion 10/Modulos/validaciones/textos.js
@@ -0,0 +1,5 @@
+"use strict";
+var MENSAJES = ["El texto es muy largo", "El texto es muy corto"];
+function obtenerError(numError) {
+    return MENSAJES[numError];
+}
---
+++ Seccion 10/Modulos/validaciones/textos.ts
@@ -0,0 +1,5 @@
+const MENSAJES: string[] = ["El texto es muy largo", "El texto es muy corto"];
+
+function obtenerError(numError: number): string {
+  return MENSAJES[numError];
+}
