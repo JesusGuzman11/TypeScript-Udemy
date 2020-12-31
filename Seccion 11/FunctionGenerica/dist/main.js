ded2228fc66689afbd1ba5e47c6c2b86937e3754
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-12-31 13:51:41 -0600

    Generic Function

---
+++ Seccion 11/FunctionGenerica/dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/* Funciones Genericas */
+function regresar(param) {
+    return param;
+}
+console.log(regresar(15.123456).toFixed(2));
+console.log(regresar("Jesus Guzman").toUpperCase());
+console.log(regresar(new Date()).toLocaleDateString());
